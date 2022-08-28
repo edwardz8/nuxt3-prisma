618074c2f824f70f799d276d82c22a08c736706a
edwardz8 <tolklorist@gmail.com>
2022-08-28 16:41:31 -0400

    revise masonry

--- components/tweet/ListFeed.vue
+++ components/tweet/ListFeed.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="columns-1 md:columns-2 lg:columns-2 xl:columns-3">
+    <div class="columns-1 sm:columns-3 md:columns-3 lg:columns-4 xl:columns-5">
         <!-- <div v-if="isEmptyArray" class="p-4">
             <p class="text-center text-gray-500">
                 No posts
             </p>
         </div> -->
-        <div v-if="tweets" class="text-center bg-gray-200 rounded-md mx-2 my-2 pb-4 cursor-pointer"
+        <div v-if="tweets" class="text-center bg-gray-200 rounded-md my-2 pb-4 cursor-pointer"
             :class="[twitterBorderColor, defaultTransition]" v-for="tweet in props.tweets" :key="tweet.id"
             @click.native="redirect(tweet)">
             <tweet-item :tweet="tweet" compact />
